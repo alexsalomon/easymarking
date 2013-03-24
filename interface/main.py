53d0715efd16f0f09caabdbb7cc5e96936424c10
Alex Salomon <alexsalomon.th@gmail.com>
2013-03-23 23:27:08 -0500

    Create basic command-line interface

---
+++ interface/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import cmd
+
+class Interface(cmd.Cmd):
+
+	intro = 'EasyMarker v1.0 - Type "help" for help.\n' \
+		'Good marking! :)\n'
+	prompt = "easyMarker=# "
+
+	def do_say(self, line):
+		"Repeats whatever the user types"
+		print line 
+
+	def default(self, line):
+		print "*** Unknown command: " + line
+
+	def do_exit(self, line):
+		return True;		
+
+	def do_quit(self, line):
+		return True;
+
+	def do_EOF(self, line):
+		print "\n"
+		return True
+
+if __name__ == '__main__':
+    Interface().cmdloop()
