f74bbfe860f7881f272637e555d47db083869f7d
Alex Salomon <umsaloma@cc.umanitoba.ca>
2013-03-26 22:46:22 -0500

    Start writting tests for newfbmsg parser

--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 decorator
 nosetest
+mock
---
+++ tests/clients/cmdline/main_tests.py
@@ -0,0 +1,9 @@
+# import nose
+# import mock
+# from clients.cmdline import main as command 
+
+# @mock.patch('engine.feedback_message.save_message')
+# def test_newfbmsg(mocked):
+# 	mocked.return_value = "Success"
+
+# 	command.CmdlineInterface().do_newfbmsg("hello ahsasaus 1")
