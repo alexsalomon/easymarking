c2b145ec73ab3b4aded8413244f2926e159a02a2
Alex Salomon <alexsalomon.th@gmail.com>
2013-04-15 17:58:50 -0500

    Complete 'generate xls course grades report' functionality

--- engine/reports/grade_reports.py
+++ engine/reports/grade_reports.py
@@ -59,6 +59,9 @@ class TxtAssignmentGradesReport(Report):
 
 class XlsCourseGradesReport(Report):
 
+	student_id_column = 0
+	first_assignment_column = 1
+
 	def __repr__(self):
 		return '<XlsCourseGradesReport assignment=%r>' % (
 			self.assignment
@@ -67,60 +70,64 @@ class XlsCourseGradesReport(Report):
 	def generate(self):
 		report_file_path = self.get_report_file_path(
 			self.assignment.course_id
-		)
-		handed_assignments = HandedAssignment.get_all(
-			self.assignment.course_id, 
-			self.assignment.number
-		)			
-		self.generate_xls_grades_file(report_file_path)
+		)		
+
+		course = Course.get(self.assignment.course_id)
+
+		book = Workbook()
+		sheet = book.add_sheet(course.course_id)
+		student_count = 1
+
+		self.write_column_titles_to_xls(sheet, course)
+
+		for student in course.students:
+			row = sheet.row(student_count)
+			self.write_student_id_to_xls(row, student)
+			self.write_student_grades_to_xls(row, student, course)
+			student_count += 1
+
+		book.save(report_file_path)
 
 	def get_report_file_path(self, course_id):
 		course_dir = self.get_course_path(course_id)
 		file_path = course_dir+'grades.xls'
 		return file_path
 
-	def generate_xls_grades_file(self, report_file_path):
-		course = Course.get(self.assignment.course_id)
-
-		book = Workbook()
-		sheet = book.add_sheet(course.course_id)
-		self.write_column_titles_to_xls(sheet, course)
-
-		book.save(report_file_path)
-
 	def write_column_titles_to_xls(self, sheet, course):
 		format = easyxf(
  			'font: name Calibri, bold on, height 240;'
  			'align: vertical center, horizontal center;'
  		)		
-		sheet.write(0, 0, 'Student ID', format)
-		sheet.write(0, 1, '', format)
-
-		column = 2
+		sheet.write(0, self.student_id_column, 'Student ID', format)
+		column = self.first_assignment_column
 
 		for assignment in course.assignments:
 			sheet.write(0, column, 'A'+str(assignment.number), format)
 			column += 1
 
+	def write_student_id_to_xls(self, row, student):
+		student_id = student.student_id
+		format = easyxf(
+ 			'font: name Calibri, height 240;'
+ 			'align: vertical center, horizontal center;'
+ 		)			
+		row.write(self.student_id_column, student_id, format)
 
-	def write_file_header(self, assignment, report_file):
-		header = "------------------------------------ Class Grades "
-		header += "------------------------------------\n"
-		header += "Course: " + self.assignment.course_id + "\n"
-		header += "Assignment: " + str(self.assignment.number) + "\n"
-		header += "Total Marks: " + str(self.assignment.maximum_marks) + "\n"
-		header += "--------------------------------------------------"
-		header += "------------------------------------\n\n"
-		header += "Student Name\t|\tGrade\n"
-		header += "-------------------------------\n"
-		report_file.write(header)
+	def write_student_grades_to_xls(self, row, student, course):
+		format = easyxf(
+ 			'font: name Calibri, height 240;'
+ 			'align: vertical center, horizontal center;'
+ 		)	
+		column = self.first_assignment_column
 
-	def write_student_header_to_file(self, handed_assignment, report_file):
-		if len(handed_assignment.student_id) > 7:
-			number_of_tabs = "\t"
-		else:
-			number_of_tabs = "\t\t"
+		for assignment in course.assignments:
+			handed_assignment = HandedAssignment.get(
+				student.student_id,
+				course.course_id,
+				assignment.number
+			)
+			row.write(column, handed_assignment.marks_achieved, format)
+			column += 1
 
-		header = handed_assignment.student_id + number_of_tabs + "|\t" + \
-			str(handed_assignment.marks_achieved) + "\n"
-		report_file.write(header)
+
+
